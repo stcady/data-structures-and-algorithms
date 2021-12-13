e36d6272f04d615fd5953e4f5d41c8ce58c03439
Steven Cady <scady@apple.com>
2021-12-12 22:03:15 -0500

    DFS and BFS

--- 9-graphs/data-structures/graph_list.py
+++ 9-graphs/data-structures/graph_list.py
@@ -27,3 +27,54 @@ graph.add_undirected_edge(0, 2)
 graph.add_undirected_edge(1, 2)
 graph.add_undirected_edge(2, 3)
 graph.print_graph()
+            
+            
+# Time-Complexity: O(V+E) in adjacency list format
+# Time-Complexity: O(V^2) in adjacency matrix format
+
+def dfs_stack(gph, source, target):
+    count = gph.count
+    visited = [False] * count
+    stk = []
+    stk.append(source)
+    visited[source] = True
+    while len(stk) != 0:
+        curr = stk.pop()
+        print(curr, end=' ')
+        for edge in gph.adj[curr]:
+            destination = edge[0]
+            if visited[destination] == False:
+                stk.append(destination)
+                visited[destination] = True
+    return visited[target]
+    
+def dfs_recurse(gph, source, target):
+    count = gph.count
+    visited = [False] * count
+    dfs_util(gph, source, visited)
+    return visited[target]
+from collections import deque
+
+def dfs_util(gph, index, visited):
+    visited[index] = True
+    for edge in gph.adj[index]:
+        destination = edge[0]
+        if visited[destination] == False:
+            dfs_util(gph, destination, visited)
+            
+def bfs(gph, source, target):
+    count = gph.count
+    visited = [False] * count
+    visited[source] = True
+    print("BFS : ", end=' ')
+    que = deque([])
+    que.append(source)
+    while len(que) != 0:
+        curr = que.popleft()
+        print(curr, end=' ')
+        for edge in gph.adj[curr]:
+            destination = edge[0]
+            if visited[destination] == False:
+                que.append(destination)
+                visited[destination] = True
+    return visited[target]
